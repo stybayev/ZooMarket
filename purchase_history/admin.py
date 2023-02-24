56cf32b0221cc07e36ecf83763da0f1bd1dec2fa
dosxana@gmail.com <dosxana@gmail.com>
2023-02-24 18:37:34 +0600

    Create example in ChatGpt

--- purchase_history/admin.py
+++ purchase_history/admin.py
@@ -1,27 +1,27 @@
-from django.contrib import admin
-from . import models
-from .models import Product
-
-
-@admin.register(models.Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        'vendor_code',
-        'title',
-        'brand',
-        'created_at')
-    list_filter = ['brand', 'title', 'vendor_code']
-    search_fields = (
-        'vendor_code',
-        'title',
-        'brand',
-    )
-    ordering = ('created_at',)
-    exclude = ('product_list',)
-
-
-@admin.register(models.ProductList)
-class ProductListAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'product')
-    list_filter = ('created_at',)
-    ordering = ('created_at',)
+# from django.contrib import admin
+# from . import models
+# from .models import Product
+#
+#
+# @admin.register(models.Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'vendor_code',
+#         'title',
+#         'brand',
+#         'created_at')
+#     list_filter = ['brand', 'title', 'vendor_code']
+#     search_fields = (
+#         'vendor_code',
+#         'title',
+#         'brand',
+#     )
+#     ordering = ('created_at',)
+#     exclude = ('product_list',)
+#
+#
+# @admin.register(models.ProductList)
+# class ProductListAdmin(admin.ModelAdmin):
+#     list_display = ('created_at', 'product')
+#     list_filter = ('created_at',)
+#     ordering = ('created_at',)
--- purchase_history/models.py
+++ purchase_history/models.py
@@ -1,102 +1,29 @@
-# from django.contrib.auth import get_user_model
-# from django.db import models
-#
-# '''
-# Модель для описания товара
-# '''
-#
-#
-# class Product(models.Model):
-#     vendor_code = models.CharField(max_length=255, null=True, blank=True, verbose_name='Артикул товара')
-#
-#     title = models.CharField(max_length=255, null=True, blank=True, verbose_name='Название товара')
-#
-#     brand = models.CharField(max_length=255, null=True, blank=True, verbose_name='Производитель/Бренд товара')
-#
-#     created_at = models.DateField(auto_now=True, verbose_name='Дата товара')
-#
-#     def __str__(self):
-#         return f'{self.title}'
-#
-#     class Meta:
-#         verbose_name = "01 Товар"
-#         verbose_name_plural = "01 Товары"
-#         ordering = ['brand']
-#
-#
-# '''
-# Модель для списка товаров (Корзина)
-# '''
-#
-#
-# class ProductList(models.Model):
-#     product = models.ForeignKey('purchase_history.Product', on_delete=models.PROTECT,
-#                                 verbose_name='Товар', related_name='product_list')
-#
-#     quantity_product = models.PositiveIntegerField(verbose_name='Количества товара')
-#
-#     price = models.DecimalField(null=True, blank=True,
-#                                 max_digits=19, decimal_places=2,
-#                                 verbose_name='Стоимость товара за 1 единицу/штуку')
-#
-#     created_at = models.DateField(auto_now=True, verbose_name='Дата создания списка товара')
-#
-#     # def __str__(self):
-#     #     return f'Список товаров покупателя {self.user} за {self.created_at}'
-#
-#     class Meta:
-#         verbose_name = "02 Список товаров"
-#         verbose_name_plural = "02 Список товаров"
-#         ordering = ['created_at']
-#
-#
-# '''
-# Модель для стоимости товара
-# '''
-#
-#
-# class PriceProduct(models.Model):
-#     product = models.ForeignKey('purchase_history.Product', on_delete=models.CASCADE,
-#                                 verbose_name='Товар', related_name='prices')
-#
-#     price = models.DecimalField(null=True, blank=True,
-#                                 max_digits=19, decimal_places=2,
-#                                 verbose_name='Стоимость товара за 1 единицу/штуку')
-#     price_date = models.DateField(null=True, blank=True, verbose_name='Дата добавления цены')
-#     created_at = models.DateField(auto_now=True, verbose_name='Дата создания цены')
-#
-#     def __str__(self):
-#         return f'{self.price} - {self.price_date}'
-#
-#     class Meta:
-#         verbose_name = "03 Стоимость товара за 1 единицу/штуку"
-#         verbose_name_plural = "03 Стоимости товаров за 1 единицу/штуку"
-#         ordering = ['price_date']
 from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
 
 '''
-В этом примере PointOfSaleмодель описывает торговую точку с nameполем. 
-Модель Purchaseописывает покупку с такими полями, как date, point_of_sale, 
-user, total_cost, discount, total_cost_with_discountи accumulated_bonuses. 
-Модель Productописывает продукт с такими полями vendor_code, как title, и brand. 
-Модель ProductInPurchaseпредставляет собой список продуктов в покупке и включает ссылку на 
-Purchaseмодель через внешний ключ и на Productмодель через другой внешний ключ. 
-Каждый ProductInPurchaseэкземпляр имеет quantityполе и priceполе для указания стоимости каждого элемента в списке.
+Модель для описания точки продаж.
+Описывает торговую точку с title полем.
 '''
 
 
 class PointOfSale(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Название точки продаж')
+    title = models.CharField(max_length=255, verbose_name='Название точки продаж')
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.title}'
 
     class Meta:
         verbose_name = "Точка продаж"
         verbose_name_plural = "Точки продаж"
+
+
+'''
+Модель Purchase описывает покупку с такими полями, как date, point_of_sale, 
+user, total_cost, discount, total_cost_with_discountи accumulated_bonuses. 
+'''
 
 
 class Purchase(models.Model):
@@ -114,17 +41,31 @@ class Purchase(models.Model):
         verbose_name_plural = "История покупок"
 
 
+'''
+Модель Product описывает продукт с такими полями vendor_code, как title, и brand. 
+'''
+
+
 class Product(models.Model):
     vendor_code = models.CharField(max_length=255, verbose_name='Артикул товара')
     title = models.CharField(max_length=255, verbose_name='Название товара')
     brand = models.CharField(max_length=255, verbose_name='Производитель/Бренд товара')
+    created_at = models.DateField(auto_now=True, verbose_name='Дата товара')
 
     def __str__(self):
         return f'{self.title}'
 
     class Meta:
-        verbose_name = "Описание товара"
-        verbose_name_plural = "Описания товаров"
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ['brand']
+
+
+'''
+Модель ProductInPurchase представляет собой список продуктов в покупке и включает ссылку на 
+Purchase модель через внешний ключ и на Product модель через другой внешний ключ. 
+Каждый ProductInPurchase экземпляр имеет quantity поле и price поле для указания стоимости каждого элемента в списке.
+'''
 
 
 class ProductInPurchase(models.Model):
